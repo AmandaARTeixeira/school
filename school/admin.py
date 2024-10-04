81eec63b2874e27c1233b1d03c5910a85aacbe89
Amanda <amandalkmim@gmail.com>
2024-10-04 19:07:15 -0300

    Create models for courses and students

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from school.models import Student, Course
 
-# Register your models here.
+class ListingStudents(admin.ModelAdmin):
+    list_display = ('id', 'name', 'email', 'cpf', 'birth_date', 'phone')
+    list_display_links = ('id', 'name',)
+    list_per_page = 20
+    search_fields = ('name',)
+
+admin.site.register(Student, ListingStudents)
+
+class ListingCourses(admin.ModelAdmin):
+    list_display = ('id', 'code', 'description', 'level')
+    list_display_links = ('id', 'code',)
+    list_per_page = 20
+    search_fields = ('code',)
+
+admin.site.register(Course, ListingCourses)
--- school/models.py
+++ school/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(blank=False, max_length=30)
+    cpf = models.CharField(max_length=11)
+    birth_date = models.DateField()
+    phone = models.CharField(max_length=14)
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Course(models.Model):
+    LEVEL = (
+        ('B', 'Basic'),
+        ('I', 'Intermediate'),
+        ('A', 'Advanced'),
+    )
+    code = models.CharField(max_length = 10)
+    description = models.CharField(max_length = 100, blank = False)
+    level = models.CharField(max_length = 1, choices = LEVEL, blank = False, null = False, default= 'B')
+
+    def __str__(self) -> str:
+        return self.code
